7ef99eb0b62efd9648fe25caa1640186bc23c584
William Johnson <willrayjohnson@gmail.com>
2018-02-22 09:46:53 -0600

    Changed PRIMARY KEY constraints

--- CREATE_FIG_1_2_DB.sql
+++ CREATE_FIG_1_2_DB.sql
@@ -6,18 +6,20 @@ USE student_course_records;
 
 CREATE TABLE Student (
     Name VARCHAR(20) NOT NULL,
-    Student_number INT NOT NULL PRIMARY KEY,
+    Student_number INT NOT NULL,
     Class INT NOT NULL,
-    Major VARCHAR(10) NOT NULL);
+    Major VARCHAR(10) NOT NULL
+    PRIMARY KEY (Student_number));
 
 CREATE TABLE Course (
     Course_name VARCHAR(40) NOT NULL,
-    Course_number VARCHAR(10) NOT NULL PRIMARY KEY,
+    Course_number VARCHAR(10) NOT NULL,
     Credit_hours INT NOT NULL,
-    Department VARCHAR(10) NOT NULL);
+    Department VARCHAR(10) NOT NULL
+    PRIMARY KEY (Course_number));
 
 CREATE TABLE Section (
-    Section_identifier INT NOT NULL PRIMARY KEY,
+    Section_identifier INT NOT NULL,
     Course_number VARCHAR(10) NOT NULL,
     FOREIGN KEY (Course_number)
         REFERENCES Course(Course_number)
@@ -25,7 +27,8 @@ CREATE TABLE Section (
         ON DELETE RESTRICT,
     Semester VARCHAR(7) NOT NULL,
     Year INT NOT NULL,
-    Instructor VARCHAR(20) NOT NULL);
+    Instructor VARCHAR(20) NOT NULL
+    PRIMARY KEY (Section_identifier));
 
 CREATE TABLE Grade_report (
     Student_number INT NOT NULL,
@@ -47,8 +50,9 @@ CREATE TABLE Prerequisite (
         REFERENCES Course(Course_number)
         ON UPDATE CASCADE
         ON DELETE RESTRICT,
-    Prequisite_number VARCHAR(10) NOT NULL,
-    FOREIGN KEY (Prequisite_number)
+    Prerequisite_number VARCHAR(10) NOT NULL,
+    FOREIGN KEY (Prerequisite_number)
         REFERENCES Course(Course_number)
         ON UPDATE CASCADE
-        ON DELETE RESTRICT);
+        ON DELETE RESTRICT
+    PRIMARY KEY (Course_number, Prerequisite_number));
